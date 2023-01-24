51722bd562ccbea8808cc08d9e29e1d4e3e3de38
SAWAN KUMAR BHAGAT <95699752+SawanKumarBhagat@users.noreply.github.com>
2023-01-24 20:27:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,15 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
+//Private Api keys
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC72CrML5ntF8Gl3MZ-KpnBg-kjCwyZNkA",
-    authDomain: "challenge-50279.firebaseapp.com",
-    projectId: "challenge-50279",
-    storageBucket: "challenge-50279.appspot.com",
-    messagingSenderId: "981466838666",
-    appId: "1:981466838666:web:f4dd73bbcac9496094df73"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
